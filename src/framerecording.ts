ba5b48c439dc8242f32f06ee7f25ea00e8c306bb
peder lexau <peder@ablemagic.no>
2022-01-28 22:05:32 +0100

    add framerecording and recording

---
+++ src/framerecording.ts
@@ -0,0 +1,7 @@
+import { Vec2 } from "./calc";
+
+export default class FrameRecording {
+    position: Vec2 = { x: 0, y: 0 }
+    rotation = 0;
+    shoot = false;
+}
---
+++ src/recording.ts
@@ -0,0 +1,34 @@
+import FrameRecording from "./framerecording";
+
+export default class Recording {
+    recordings: Array<FrameRecording> = [];
+    currentIndex = 0;
+    backwards = false;
+
+    add(frameRecording: FrameRecording) {
+        this.recordings.push(frameRecording);
+    }
+
+    get(index: number): FrameRecording {
+        return this.recordings[index];
+    }
+
+    getNext(): FrameRecording {
+        const frameRecording = this.recordings[this.currentIndex];
+        
+        if (this.backwards) {
+            this.currentIndex--;
+            if (this.currentIndex == 0) {
+                this.backwards = false;
+            }
+        }
+        else {
+            this.currentIndex++;
+            if (this.currentIndex == this.recordings.length - 1) {
+                this.backwards = true;
+            }
+        }
+
+        return frameRecording;
+    }
+}
