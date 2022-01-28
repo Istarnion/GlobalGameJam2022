99b846ff6b8e11ae8d2934247d578c82512856c1
istarnion <hallgeir@loekken.org>
2022-01-28 21:09:59 +0100

    Make project run

---
+++ src/gamestates/battlestate.ts
@@ -0,0 +1,8 @@
+import GameState from "../gamestate";
+
+export default class BattleState extends GameState {
+
+    constructor() {
+        super();
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 import Game from "./game";
-import { TestState } from "./gamestates/teststate";
+import BattleState from "./gamestates/battlestate";
 import { loadImages, loadSprites, loadMap, loadFont } from "./loader";
 
 /**
  * Global game object. Through this we can
  * access the stuff we need global access to.
  */
-const game = new Game(120, 80, new TestState());
+const game = new Game(120, 80, new BattleState());
 
 export default game;
 
-const preloads = [
-    loadImages([
-        ['ground', './res/test_tileset8.png'],
-        ['testcharacter', './res/testcharacter.png']
-    ]),
-    loadMap('testmap', './res/testmap.json'),
-    loadSprites('./res/sprites.json'),
-    loadFont('express_mono', './licenced_res/express_mono/metrics.txt', './licenced_res/express_mono/atlas32.png')
+const preloads: Promise<any>[] = [
 ];
 
 Promise.all(preloads).then((loadedAssets) => {
