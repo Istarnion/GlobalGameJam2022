b1dd64398e560735de4c0ca29235d69fac64072c
istarnion <hallgeir@loekken.org>
2022-01-29 11:20:38 +0100

    Implement camera for battle state

--- src/gamestates/battlestate.ts
+++ src/gamestates/battlestate.ts
@@ -4,12 +4,15 @@ import Player from "../components/player";
 import Entity from "../entity";
 import { createPlayer } from "../factory";
 import GameState from "../gamestate";
+import gfx from "../graphics";
 import World from "../world";
 
 export default class BattleState extends GameState {
 
     world: World;
     player: Entity;
+    cameraX = 0;
+    cameraY = 0;
 
     constructor() {
         super();
@@ -26,6 +29,9 @@ export default class BattleState extends GameState {
     }
 
     override draw(): void {
+        gfx.save();
+        gfx.translate(gfx.width/2-this.cameraX, gfx.height/2-this.cameraY);
         this.world.render();
+        gfx.restore();
     }
 }
