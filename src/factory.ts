38b605c6f8cc4732abe92ab551f3232cad15a9ab
istarnion <hallgeir@loekken.org>
2022-01-30 01:13:01 +0100

    Change color of enemy projectile

--- src/factory.ts
+++ src/factory.ts
@@ -47,7 +47,7 @@ export function createPlayer(x: number, y: number, world: World): Entity {
 
 export function createPeaProjectile(x: number, y: number, angle: number, speed: number, mask: Mask, world: World): Entity {
     const projectile = world.addEntity(x, y);
-    const projectileColor = mask === Mask.PLAYER_PROJECTILE ? 'lime' : '#73339D';
+    const projectileColor = mask === Mask.PLAYER_PROJECTILE ? 'lime' : '#bc90da';
     const pea = projectile.add(new PeaProjectile(projectileColor, projectile));
     angle -= TAU / 4;
     pea.dx = Math.cos(angle) * speed;
